c5f21df5f8f6dbe4b91268760ed0627b95d71370
Madhavi Sattiraju <52825067+MadhaviSattiraju@users.noreply.github.com>
2020-10-12 22:40:01 +0530

    functions basics

---
+++ functions.py
@@ -0,0 +1,24 @@
+def add():
+    a=int(input("Enter a value:"))
+    b=int(input("Enter b value:"))
+    print(a+b)
+add()
+
+add()
+add()
+
+
+def add():
+    a=int(input("Enter a value:"))
+    b=int(input("Enter b value:"))
+    return(a+b)
+print(add())
+
+
+def add(a,b):
+        return(a+b)
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+print(add(a,b))
+
+print(add(99,90))
